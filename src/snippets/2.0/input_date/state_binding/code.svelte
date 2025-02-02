49075540606e75b709f381b6759831144434b6ca
Sveltecult <sveltecult@proton.me>
2025-02-02 17:43:24 +0800

    update Calendar, Input Date, Input Time, Select

---
+++ src/snippets/2.0/input_date/state_binding/code.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let value: string = $state('');
+
+	onMount(() => {
+		document.getElementById('date')?.addEventListener('uk-input-date:change', (e) => {
+			// @ts-ignore
+			value = e.detail.value;
+		});
+	});
+</script>
+
+<div class="uk-container mt-20 grid grid-cols-2 gap-4">
+	<div>
+		<div class="uk-h3">Form</div>
+		<div class="mt-4">
+			<uk-input-date id="date"></uk-input-date>
+		</div>
+	</div>
+	<div>
+		<div class="uk-h3">Result</div>
+		{#if value}
+			The date is: {value}
+		{/if}
+	</div>
+</div>
---
+++ src/snippets/2.0/input_time/state_binding/code.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let value: string = $state('');
+
+	onMount(() => {
+		document.getElementById('time')?.addEventListener('uk-input-time:change', (e) => {
+			// @ts-ignore
+			value = e.detail.value;
+		});
+	});
+</script>
+
+<div class="uk-container mt-20 grid grid-cols-2 gap-4">
+	<div>
+		<div class="uk-h3">Form</div>
+		<div class="mt-4">
+			<uk-input-time id="time"></uk-input-time>
+		</div>
+	</div>
+	<div>
+		<div class="uk-h3">Result</div>
+		{#if value}
+			The time is: {value}
+		{/if}
+	</div>
+</div>
